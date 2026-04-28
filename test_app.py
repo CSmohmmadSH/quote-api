6547cf40383991ce1fd15a898fcbc7b611d059b2
Mohammad <mohmmad521@hotmail.com>
2026-04-28 19:06:42 +0300

    Add /count endpoint with test

--- test_app.py
+++ test_app.py
@@ -8,3 +8,7 @@ def test_get_random_quote_returns_string_from_quotes():
 
 def test_quotes_list_is_not_empty():
     assert len(quotes) > 0, "Should have at least one quote"
+
+
+def test_quotes_list_has_expected_minimum():
+    assert len(quotes) >= 3, "Should have at least 3 quotes"
